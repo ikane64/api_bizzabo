d124fd9aebcbb83c73862a384ef36888eacf4a70
ikane64 <kaneigor64@gmail.com>
2024-01-30 16:35:38 +0200

    button disable

--- public/JS/exportMagicLinks.js
+++ public/JS/exportMagicLinks.js
@@ -1,41 +1,50 @@
 async function exportMagicLinks() {
-  try {
-      const apiKey = document.getElementById('apiKey').value;
-      const eventId = document.getElementById('eventId').value;
+    try {
 
-      const response = await fetch('/linkExport', {
-          method: 'POST',
-          body: JSON.stringify({
-              apiKey,
-              eventId
-          }), // Use the extracted values from the form
-          headers: {
-              'Content-Type': 'application/json'
-          }
-      });
+        // Disable the submit button to prevent multiple clicks
+        const submitButton = document.querySelector('button[type="submit"]');
+        submitButton.disabled = true;
+        submitButton.textContent = 'Processing...';
 
-      const data = await response.json();
-      const resultDiv = document.getElementById('result');
-      let fileLinkLabel = document.getElementById('fileLink');
+        const apiKey = document.getElementById('apiKey').value;
+        const eventId = document.getElementById('eventId').value;
 
-      if (!fileLinkLabel) {
-          // If the fileLink label element doesn't exist, create one
-          fileLinkLabel = document.createElement('label');
-          fileLinkLabel.setAttribute('class', 'form-control output');
-          fileLinkLabel.setAttribute('id', 'fileLink');
-          fileLinkLabel.setAttribute('name', 'fileLink');
-          resultDiv.appendChild(fileLinkLabel);
-      }
+        const response = await fetch('/linkExport', {
+            method: 'POST',
+            body: JSON.stringify({
+                apiKey,
+                eventId
+            }), // Use the extracted values from the form
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-      if (data.fileLink) {
-          // If the file link is available, update the label element with the link
-          fileLinkLabel.innerHTML = `Link export was successful. Download the file <a href="${data.fileLink}">here</a>.`;
-      } else {
-          // If there is no file link, display a message indicating no registrations were processed
-          fileLinkLabel.textContent = data.message;
-      }
-  } catch (error) {
-      console.error('Error processing registrations:', error);
-      document.getElementById('result').textContent = 'An error occurred while processing registrations.';
-  }
+        const data = await response.json();
+        const resultDiv = document.getElementById('result');
+        let fileLinkLabel = document.getElementById('fileLink');
+
+        if (!fileLinkLabel) {
+            // If the fileLink label element doesn't exist, create one
+            fileLinkLabel = document.createElement('label');
+            fileLinkLabel.setAttribute('class', 'form-control output');
+            fileLinkLabel.setAttribute('id', 'fileLink');
+            fileLinkLabel.setAttribute('name', 'fileLink');
+            resultDiv.appendChild(fileLinkLabel);
+        }
+
+        if (data.fileLink) {
+            // If the file link is available, update the label element with the link
+            fileLinkLabel.innerHTML = `Link export was successful. Download the file <a href="${data.fileLink}">here</a>.`;
+        } else {
+            // If there is no file link, display a message indicating no registrations were processed
+            fileLinkLabel.textContent = data.message;
+        }
+
+        submitButton.disabled = false;
+        submitButton.textContent = 'Export Links';
+    } catch (error) {
+        console.error('Error processing registrations:', error);
+        document.getElementById('result').textContent = 'An error occurred while processing registrations.';
+    }
 }
