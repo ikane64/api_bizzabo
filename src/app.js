3d1eb09bdf53ed841ab8530bd077850110bdb0b1
ikane64 <kaneigor64@gmail.com>
2024-02-16 21:02:10 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 const getLinkController = require('./controllers/getLinkController');
 const getLinkListController = require('./controllers/getLinkListController');
+const updateFieldController = require('./controllers/updateFieldController');
+const bulkCancelController = require('./controllers/bulkCancelController');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
+const http = require('http');
 
+const options = {
+  key: null,
+  cert: null
+};
+
+/*
+try {
+  options.key = fs.readFileSync('/etc/letsencrypt/live/bizzaboapitest.online/privkey.pem');
+  options.cert = fs.readFileSync('/etc/letsencrypt/live/bizzaboapitest.online/fullchain.pem');
+} catch (err) {
+  console.error('Error reading key/cert files:', err);
+  // Handle the error, such as exiting the application or providing a default key/cert
+}
+*/
 const app = express();
 
 app.use(bodyParser.urlencoded({
@@ -25,12 +45,20 @@ app.get('/linkExport', (req, res) => {
 app.get('/fieldUpdate', (req, res) => {
     res.render('fieldUpdate.ejs');
 });
+app.get('/bulkCancel', (req, res) => {
+  res.render('bulkCancel.ejs');
+});
 
 app.post('/', getLinkController.getMagicLink);
 app.post('/linkExport', getLinkListController.getRegistrations);
+app.post('/fieldUpdate', updateFieldController.updateField);
+
+app.post('/bulkCancel', bulkCancelController.bulkTicketCancel);
+app.post('/cancel-bulk-cancelation', bulkCancelController.stopBulkCancellation);
 
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const httpsServer = http.createServer(app);
+
+httpsServer.listen(3000, () => {
+  console.log('Server is running on port 443');
 });
